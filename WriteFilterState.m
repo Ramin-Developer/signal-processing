344e9187ba778d4b18da73988ab561f77bb15578
Ramin Anvar <ramina1964@gmail.com>
2026-08-27 15:44:05 +0200

    Validate filter state output paths

--- WriteFilterState.m
+++ WriteFilterState.m
@@ -3,6 +3,16 @@ function WriteFilterState(inputPath, outputPath, inputSignal, outputSignal)
 
 inputHandle = fopen(inputPath, 'wt');
 outputHandle = fopen(outputPath, 'wt');
+if inputHandle == -1 || outputHandle == -1
+	if inputHandle ~= -1
+		fclose(inputHandle);
+	end;
+	if outputHandle ~= -1
+		fclose(outputHandle);
+	end;
+	error('SignalProcessing:StateFileOpenFailed', ...
+		'Unable to open the persisted filter state files for writing.');
+end;
 fprintf(inputHandle, '%-16.12g\n', inputSignal);
 fprintf(outputHandle, '%-16.12g\n', outputSignal);
 fclose(inputHandle);
